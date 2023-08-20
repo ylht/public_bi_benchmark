89b1369aff2b597b618d8f702198f6b2d2944f2f
ylht <wqs.97@icloud.com>
2023-08-20 21:00:30 +0800

    [init] init classify path

--- src/main/java/ecnu/db/ClassifyWorkload.java
+++ src/main/java/ecnu/db/ClassifyWorkload.java
@@ -1,7 +1,10 @@
 package ecnu.db;
 
-public class ClassifyWorkload {
+import java.nio.file.Path;
 
+public class ClassifyWorkload {
+    private static final Path transferWorkloadPath = Path.of("benchmark-transferred");
+    private static final Path classifyWorkloadPath = Path.of("benchmark-classified");
     public static void main(String[] args) {
 
     }
